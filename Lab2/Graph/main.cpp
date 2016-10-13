979d910ea32951951de8ece6403e321760f4651c
Park Jee Hyun <jhyun19@gmail.com>
2016-10-13 15:07:07 +0800

    lab2
    -graph
    -dfs

--- Lab2/Graph/main.cpp
+++ Lab2/Graph/main.cpp
@@ -1,22 +1,60 @@
 #include <stdio.h>
 
-#define maxV 100
+#define maxV 20
 
-int V, E, x, y;
-int a[maxV][maxV];
+int V = 1;
+int x, y;
+int Graph[maxV][maxV];
+int TreeGraph[maxV][maxV];
 int count = 0;
+int component = 0;
 
-void read_graph(void);
+void build_graph(void);
 
 void print_graph(void);
 
-void main() {
-    read_graph();
+void DFS(int G[maxV][maxV]);
+
+void dfs(int x, int y);
+
+int main() {
+    build_graph();
     print_graph();
+
+    return 0;
+}
+
+
+void DFS(int G[maxV][maxV]) {
+
+    for (x = 1; x <= V; x++) {
+        for (y = 1; y <= V; y++) {  // for each vertex
+
+            if (0 == G[x][y]) {
+                component = component + 1;
+                dfs(x, y);
+            }
+        }
+    }
+}
+
+void dfs(int x, int y) {
+    int i;
+
+    count = count + 1;
+    Graph[x][y] = count;    // mark the vertex v with "count"
+
+    for (i = 1; i <= 7; i++) {  //for each vertex w, adjacent with to v
+        if (i != y) {
+            if(Graph[x][i] == 0){
+                dfs(x, i);
+            }
+        }
+    }
 
 }
 
-void read_graph(void) {
+void build_graph(void) {
     int edge, x;
     printf("\nInput number of vertices :");
     scanf("%d", &V);
@@ -26,7 +64,7 @@ void read_graph(void) {
     }
     else {
         for (x = 1; x <= V; x++) {
-            for (y = 1; y <= V; y++){
+            for (y = 1; y <= V; y++) {
                 a[x][y] = 0;
             }
         }
@@ -47,7 +85,7 @@ void print_graph(void) {
     for (x = 1; x <= V; x++) {
 
         for (y = 1; y <= V; y++) {
-            printf("a[ %d ][ %d ]= %d", x, y, a[x][y]);
+            printf("a[ %d ][ %d ]= %d \n", x, y, a[x][y]);
         }
     }
 }
